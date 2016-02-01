2a0d41e90bd875f75e6013245d139f4a2e4c7501
chris <chris@chriss-MacBook-Pro.local>
2016-02-01 18:01:05 +0000

    q12

--- app.rb
+++ app.rb
@@ -7,8 +7,13 @@ get '/secret' do
   "this is an secret message"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
-  <div>"
+get '/random-cat' do
+  @name = ["James","Peter","tom"].sample
+erb(:index)
+end
+
+get '/named-cat' do
+  #p params
+  @name = params[:name]
+erb(:index)
 end
